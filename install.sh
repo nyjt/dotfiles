db2375f9a10741fef55982ba3ea01b2aa7c2dc45
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2013-11-12 09:07:15 +0100

    removing sampler-pack related hack from install script

--- install.sh
+++ install.sh
@@ -44,6 +44,3 @@ curl -Sso ~/.vim/autoload/pathogen.vim \
 
 git config --global core.excludesfile ~/.gitignore_global
 
-solarized_color_file=$dotfiles_path/vim/colors/sampler-pack/colors/solarized.vim
-[ -e $solarized_color_file ] && rm $solarized_color_file
-
