f4e73be64c7f5a39e4a02757aaf26e74ede59538
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-02-06 10:31:41 +0100

    installer script for ubuntu

---
+++ ubuntu-laptop-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Ubuntu packages install
+sudo apt-get --yes install default-jre vim curl postgresql git tig mosh ubuntu-desktop chromium-browser libyaml-0-2 rar octave whois
+
+# RVM install
+if type rvm | head -n 1
+then
+  echo 'RVM has already installed.'
+else
+  curl -sSL https://get.rvm.io | bash -s stable --rails
+  source ~/.rvm/scripts/rvm
+fi
+
