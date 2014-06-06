1ee6db2f8d1a1313fb43c0f354587631ffdc5f64
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-06-06 10:35:16 +0200

    Install Google Fonts to Ubuntu

--- install.sh
+++ install.sh
@@ -30,17 +30,52 @@ function _dotfile_create_symlink {
   fi
 }
 
+
+# Original author: Michalis Georgiou <mechmg93@gmail.comr>
+# Modified by Andrew http://www.webupd8.org <andrew@webupd8.org>
+function install_google_fonts {
+  sudo apt-get install -y mercurial
+
+  _hgroot='https://googlefontdirectory.googlecode.com/hg/'
+  _hgrepo='googlefontdirectory'
+
+  echo 'Connecting to Mercurial server....'
+  if [ -d $_hgrepo ]; then
+    cd $_hgrepo
+    hg pull -u || return 1
+    echo 'The local files have been updated.'
+    cd ..
+  else
+    hg clone $_hgroot $_hgrepo || return 1
+  fi
+  echo 'Mercurial checkout done or server timeout'
+  sudo mkdir -p /usr/share/fonts/truetype/google-fonts/
+  find $PWD/$_hgrepo/ -name '*.ttf' -exec sudo install -m644 {} /usr/share/fonts/truetype/google-fonts/ \; || return 1
+  fc-cache -f >/dev/null
+  echo 'done.'
+}
+
+echo 'Creating missing symlinks.'
 _dotfile_create_symlink vim
 for file_name in profile vimrc gemrc gitignore_global rspec bashrc rubocop.yml
 do
   _dotfile_create_symlink $file_name
 done
 
+echo 'Initializing and upgrading git submodules.'
 git submodule init
 git submodule update
 
+echo 'Installing pathogen.vim.'
 curl -Sso ~/.vim/autoload/pathogen.vim \
   https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+echo 'Setting global gitignore as ~/.gitignore_global.'
 git config --global core.excludesfile ~/.gitignore_global
+
+if uname -a | grep Ubuntu
+then
+  echo 'Installing Google fonts.'
+  install_google_fonts
+fi
 
